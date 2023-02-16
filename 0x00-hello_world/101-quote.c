757170e29179ae37de612e0c44f259586adad274
cleokibet <cleokibet@gmail.com>
2023-02-16 12:08:34 -0800

    ./quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * main - print "and that piece of art is useful" - Dora karpa, 2015 - 10 - 19"
+ * followed by a new line, to standard error.
+ * return: always 1.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" -Dora kopar, 2015-10-19\n", 59);
+	return (1);
+}
