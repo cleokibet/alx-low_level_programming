d910f2cba83d2adc145dcc8784b3433afc197d89
cleokibet <cleokibet@gmail.com>
2023-02-19 10:32:58 -0800

    ./101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/*
+ * main -prints all possible different combinations of three digits
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int s = 0, e = 7, s2 = 1, e2 = 8, s3 = 2, e3 = 9;
+
+	while (s <= e)
+	{
+	s2 = s + 1;
+	while (s2 <= e2)
+		{
+		s3 = s + 1;
+		while (s3 <= e3)
+			{
+			putchar(s + '0');
+			putchar(s2 + '0');
+			putchar(s3 + ('0'));
+			if (s != e)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+				s3++;
+			}
+			s2++;
+		}
+		s++;
+	}
+	putchar('\n');
+
+	return (0);
+}
