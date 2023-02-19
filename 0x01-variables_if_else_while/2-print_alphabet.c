a7fe0bae8254bbb9887ee885b7ed53e3b8a69907
cleokibet <cleokibet@gmail.com>
2023-02-19 02:38:27 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,15 +4,15 @@
 *
 * Return: Always 0 (success)
 */
-int main (void)
+int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	
+
 	for (i + 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
