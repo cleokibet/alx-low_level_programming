8973c00aa9fddd633fe925606f7586f26d3c7820
cleokibet <cleokibet@gmail.com>
2023-02-19 03:42:19 -0800

    print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Prints alphabet in reverse
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char le;
+
+	for (le = 'z'; le <= 'z'; le--)
+		putchar(le);
+
+	putchar('\n');
+
+	return (0);
+}
