8e3909103bacf75a8526b7de2f059f50b1609947
cleokibet <cleokibet@gmail.com>
2023-02-23 13:16:37 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* mul - Multiplies two digits
+* @x: The first number to multiply
+* @y: the second number to multiply with first number.
+* Return: the results of multiplication between x and y param
+*/
+
+int mul(int x, int y)
+{
+return (x * y);
+}
+
