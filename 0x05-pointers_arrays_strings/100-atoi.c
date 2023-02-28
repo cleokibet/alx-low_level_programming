ba96dc0af2862d83831f6c6b2f6ce38c78f97922
cleokibet <cleokibet@gmail.com>
2023-02-28 08:21:56 -0800

    100-main.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,22 +4,24 @@
  * @s: the pointer tomconvert
  * Return: an integer
  */
-int _atoi(chr *s)
+int _atoi(char *s)
 {
 int c = 0;
 unsigned int ni = 0;
+int min = 1;
 int isi = 0;
+
 while (s[c])
 {
-if ([c] == 45)
+if (s[c] == 45)
 {
-main *= -1;
+min *= -1;
 }
 while (s[c] >= 48 && s[c] <= 57)
 {
 isi = 1;
 ni = (ni * 10) + (s[c] - '0');
-c++
+c++;
 }
 if (isi == 1)
 {
