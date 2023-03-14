6b4036894b7a7c9724ef3451ddb161a6bc266560
cleokibet <cleokibet@gmail.com>
2023-03-14 23:20:10 +0300

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,65 +1,27 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _strlen - count array
- * @s: arrays of elements
- * Return: 1
- */
-
-int _strlen(char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (s[i] != '\0') /*count character of string*/
-	{
-		i++;
-	}
-	return (i);
-}
-/**
- * _strcpy - copy arrays
- * @src: arrays of elements
- * @dest: dest array
- * Return: dest
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-/**
- * _strdup - arrays for print astring
- * @str: arrays of elements
- * Return: pointer
+ * *_strdup - copies the string given as parameter
+ * @str: string to duplicate
+ *
+ * Return: points to the copied string (success), null (error)
  */
 
 char *_strdup(char *str)
 {
-	char dst;
-	unsigned int size;
+	char *duplicate;
+	unsigned int i, len;
 
-	if (str == 0)
-	{
+	i = 0;
+	len = 0;
+	if (str == NULL)
+	return (NULL);
+	while (str[len])
+		duplicate = malloc(sizeof(char) * (len + 1));
+	if (duplicate == NULL)
 		return (NULL);
-	}
-	size = _strlen(str) + 1;
-	dst = (char *) malloc(size * sizeof(char));
-
-	if (dst == 0)
-	{
-		return (NULL);
-	}
-	_strcpy(dst, str);
-	return (dst);
+	while ((duplicate[i] = str[i]) != '\0')
+		i++;
+	return (duplicate);
 }
-
-
-
