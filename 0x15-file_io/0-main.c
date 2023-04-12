fdc84ec1c598258f374b808b8d030e2f018a700f
cleokibet <cleokibet@gmail.com>
2023-04-12 23:53:29 +0300

    update

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+#include <unistd.h>
 
 /**
 * main - check the code
@@ -10,16 +11,11 @@
 */
 int main(int ac, char **av)
 {
-ssize_t n;
 
 if (ac != 2)
 {
 dprintf(2, "Usage: %s filename\n", av[0]);
 exit(1);
 }
-n = read_textfile(av[1], 114);
-printf("\n(printed chars: %li)\n", n);
-n = read_textfile(av[1], 1024);
-printf("\n(printed chars: %li)\n", n);
 return (0);
 }
--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 /**
 * read_textfile- Read text file print to STDOUT.
 * @filename: text file being read
@@ -10,15 +13,36 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buf;
-ssize_t fd;
-ssize_t w;
-ssize_t t;
+ssize_t fd, t, w;
+
+if (filename == NULL)
+	return (0);
+
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
+
 buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+{
+	close(fd);
+	return (0);
+}
 t = read(fd, buf, letters);
+if (t == -1)
+{
+	free(buf);
+	close(fd);
+	return (0);
+}
+
 w = write(STDOUT_FILENO, buf, t);
+if (w == -1)
+{
+free(buf);
+close(fd);
+return (w);
+}
 free(buf);
 close(fd);
 return (w);
