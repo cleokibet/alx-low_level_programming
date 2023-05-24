fe88ae4eaef3de6743c6cc31270321d9c12c00ba
cleokibet <cleokibet@gmail.com>
2023-05-24 05:54:43 +0300

    advanced 1

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,22 @@
+// operations.c
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+float divide(float a, float b) {
+    return a / b;
+}
+
+int mod(int a, int b) {
+    return a % b;
+}
+
