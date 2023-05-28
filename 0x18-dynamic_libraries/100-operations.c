e25ca2bdbec8e7c33761c3e361a405ef4a964cf7
cleokibet <cleokibet@gmail.com>
2023-05-28 20:37:26 +0300

    update

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,23 +1,51 @@
 #include <stdio.h>
-// operations.c to transform
-
-int add(int a, int b) {
-    return a + b;
+/**
+ * add - adds two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: sum of the integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
 }
-
-int subtract(int a, int b) {
-    return a - b;
+/**
+ * sub - subtracts two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: difference of two integers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
 }
-
-int multiply(int a, int b) {
-    return a * b;
+/**
+ * mul - multiplies two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: product of two integers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
 }
-
-float divide(float a, float b) {
-    return a / b;
+/**
+ * div - divides two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: division of two integers
+ */
+int div(int a, int b)
+{
+	return (a / b);
 }
-
-int mod(int a, int b) {
-    return a % b;
+/**
+ * mod - determines modulus of two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: modulus of two integers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
 }
-
